aba734f852d3dbbb67fc3211a21017e8fa3b518d
Navneet Sharma <ns.jagner@gmail.com>
2025-04-19 10:05:03 +0530

    update

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favorite food here</h2>
+        <p>A website selling plants should offer a user-friendly platform for browsing, selecting, and purchasing a variety of plants, including indoor and outdoor varieties. It should also provide clear product descriptions, high-quality images, and secure payment options. The website should also offer accurate and reliable information about the plants, including their care requirements and suitability for different environments.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
